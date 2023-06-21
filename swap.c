bd558e6f7a1adcc1a6caef27d810409957883a14
jules ntizimira <ntizimijules@gmail.com>
2023-06-21 15:56:48 +0000

    swap.c

--- swap.c
+++ swap.c
@@ -1,12 +1,14 @@
 # include "monty.h"
-
+/**
+ * _swap - swap 2top elements
+ * @h: input node
+ * @n: data;
+ */
 void _swap(stack_t **h, int n)
 {
-
 	int tmp = (*h)->n;
-	
-	(void)n;
 
+	(void)n;
 	(*h)->n = (*h)->next->n;
 	(*h)->next->n = tmp;
 }
