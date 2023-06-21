e673e7f6dc63d1e8608dc580d86c11dbd707056e
jules ntizimira <ntizimijules@gmail.com>
2023-06-21 15:52:47 +0000

    betty pop.c

--- pop.c
+++ pop.c
@@ -1,11 +1,14 @@
 # include "monty.h"
-
+/**
+ * _pop - pint
+ * @h: input node
+ * @n: data;
+ */
 void _pop(stack_t **h, int n)
 {
 	stack_t *tmp = *h;
 
 	(void)n;
-
 	if (tmp->next != NULL)
 	{
 		*h = tmp->next;
