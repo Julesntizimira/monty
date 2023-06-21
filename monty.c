971b0c9455e8bf7f044ed49546db733fe9954548
jules ntizimira <ntizimijules@gmail.com>
2023-06-21 21:10:00 +0000

    fflush

--- monty.c
+++ monty.c
@@ -184,6 +184,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 2)
 	{
+		fflush(stderr);
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
@@ -191,6 +192,7 @@ int main(int argc, char *argv[])
 
 	if (access(path, R_OK) != 0)
 	{
+		fflush(stderr);
 		fprintf(stderr, "Error: Can't open file %s\n", path);
 		exit(EXIT_FAILURE);
 	}
